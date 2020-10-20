628ef734a0725b294bea1d278a15a3212bbdbf41
qadyr <44341099+qadyr@users.noreply.github.com>
2020-10-21 00:55:34 +0600

    010_cerave(3) finished

--- 010_cerave(2)/js/common.js
+++ 010_cerave(2)/js/common.js
@@ -12,34 +12,6 @@ $(document).ready(function () {
     $('.main-m').removeClass('hide');
   };
 
-  var changeBGtoMainColor = function() {
-    $('.wrap .common-bg').removeClass('hide');
-  };
-
-  var changeBGtoWB = function() {
-    $('.wrap .common-bg1add').removeClass('hide');
-  };
-
-  var removeWB = function() {
-    $('.wrap .common-bg1add').addClass('hide');
-  };
-
-  var changeBGtoWhite = function() {
-    $('.wrap .common-bg2').removeClass('hide');
-  };
-
-  var removeWhite = function() {
-    $('.wrap .common-bg2').addClass('hide');
-  };
-
-  var changeBGtoGB = function() {
-    $('.wrap .common-bg3').removeClass('hide');
-  };
-
-  var removeGB = function() {
-    $('.wrap .common-bg3').addClass('hide');
-  };
-
   var it1 = 768 / 100;
   var it2;
   var it4 = 1024 / 100;
@@ -100,37 +72,53 @@ $(document).ready(function () {
     $('.sl2 .vec4').addClass('ac');
   });
 
+  var changeBGtoWhite = function() {
+    $('.wrap .common-bg1').addClass('hide');
+  };
+
+  var removeWhite = function() {
+    $('.wrap .common-bg1').removeClass('hide');
+  };
+
+  var changeBGtoFirstColor = function() {
+    $('.wrap .common-bg2').removeClass('hide');
+  };
+
+  var removeFirstColor = function() {
+    $('.wrap .common-bg2').addClass('hide');
+  };
+
   $('.sl2 .vec1 a').click(function() {
     $('.m_pop1').fadeIn();
-    hideEl();
     changeBGtoWhite();
+    hideEl();
   });
   $('.m_pop1 .x').click(function() {
     $(this).parent().fadeOut();
-    showEl();
     removeWhite();
+    showEl();
   });
 
   $('.sl2 .vec2 a').click(function() {
     $('.m_pop2').fadeIn();
+    changeBGtoFirstColor();
     hideEl();
-    changeBGtoWB();
   });
   $('.m_pop2 .x').click(function() {
     $(this).parent().fadeOut();
+    removeFirstColor();
     showEl();
-    removeWB();
   });
 
   $('.sl2 .vec3 a').click(function() {
     $('.m_pop3').fadeIn();
+    changeBGtoFirstColor();
     hideEl();
-    changeBGtoGB();
   });
   $('.m_pop3 .x').click(function() {
     $(this).parent().fadeOut();
+    removeFirstColor();
     showEl();
-    removeGB();
   });
 
   $('.sl2 .vec4 a').click(function() {
@@ -139,106 +127,20 @@ $(document).ready(function () {
     hideEl();
   });
 
-  var changeBGtoFirstBG = function() {
-    $('.wrap .common-bg4').removeClass('hide');
-  };
-
-  var removeFirstBG = function() {
-    $('.wrap .common-bg4').addClass('hide');
-  };
-
-  var changeBGtoSecondBG = function() {
-    $('.wrap .common-bg5').removeClass('hide');
-  };
-
-  var removeSecondBG = function() {
-    $('.wrap .common-bg5').addClass('hide');
-  };
-
-  var changeBGtoThirdBG = function() {
-    $('.wrap .common-bg6').removeClass('hide');
-  };
-
-  var removeThirdBG = function() {
-    $('.wrap .common-bg6').addClass('hide');
-  };
-
-  var changeBGtoFourthBG = function() {
-    $('.wrap .common-bg7').removeClass('hide');
-  };
-
-  var removeFourthBG = function() {
-    $('.wrap .common-bg7').addClass('hide');
-  };
-
   $('.m_pop3 .o1').click(function() {
     $('.x_pop1').fadeIn();
-    changeBGtoFirstBG();
   });
 
   $('.x_pop1 .x').click(function() {
     $(this).parent().fadeOut();
-    removeFirstBG();
   });
 
   $('.m_pop3 .o2').click(function() {
     $('.x_pop2').fadeIn();
-    changeBGtoSecondBG();
   });
 
   $('.x_pop2 .x').click(function() {
     $(this).parent().fadeOut();
-    removeSecondBG();
-  });
-
-  $('.m_pop3 .o3').click(function() {
-    $('.x_pop3').fadeIn();
-    changeBGtoFirstBG();
-  });
-
-  $('.x_pop3 .x').click(function() {
-    $(this).parent().fadeOut();
-    removeFirstBG();
-  });
-
-  $('.m_pop3 .o4').click(function() {
-    $('.x_pop4').fadeIn();
-    changeBGtoThirdBG();
-  });
-
-  $('.x_pop4 .x').click(function() {
-    $(this).parent().fadeOut();
-    removeThirdBG();
-  });
-
-  $('.m_pop3 .o5').click(function() {
-    $('.x_pop4').fadeIn();
-    changeBGtoThirdBG();
-  });
-
-  $('.x_pop5 .x').click(function() {
-    $(this).parent().fadeOut();
-    removeThirdBG();
-  });
-
-  $('.m_pop3 .o6').click(function() {
-    $('.x_pop4').fadeIn();
-    changeBGtoThirdBG();
-  });
-
-  $('.x_pop6 .x').click(function() {
-    $(this).parent().fadeOut();
-    removeThirdBG();
-  });
-
-  $('.m_pop3 .o7').click(function() {
-    $('.x_pop7').fadeIn();
-    changeBGtoFourthBG();
-  });
-
-  $('.x_pop7 .x').click(function() {
-    $(this).parent().fadeOut();
-    removeFourthBG();
   });
 
   $('.main-m .prev-move').click(function() {
